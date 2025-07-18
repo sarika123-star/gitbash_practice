5b42936410c5dc78f3cbd3b1e31e04cb58db0dfb
sarika123-star <sarika.rpea29@gmail.com>
2025-07-18 17:46:06 +0530

    first push

---
+++ src/main/java/github/hi_GitHUB.java
@@ -0,0 +1,9 @@
+package github;
+
+public class hi_GitHUB {
+
+	public static void main(String[] args) {
+		System.out.println("hi github");
+	}
+
+}
---
+++ src/test/java/githubb/bye_github.java
@@ -0,0 +1,10 @@
+package githubb;
+
+public class bye_github {
+
+	public static void main(String[] args) {
+		System.out.println("bye github");
+
+	}
+
+}
